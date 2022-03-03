e7ac3738c160bce1d858ecd1cb4337bc4b0b5d81
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-03 21:25:26 +0800

    Vue-axios 自己编写的js文件

---
+++ Vue-axios/js/main.js
@@ -0,0 +1,23 @@
+/*
+  请求地址:http://wthrcdn.etouch.cn/weather_mini
+  请求方法:get
+  请求参数:city(城市名)
+  响应内容:天气信息
+
+  1. 点击回车
+  2. 查询数据
+  3. 渲染数据
+*/
+
+let vue = new Vue({
+    el: "#app",
+    data: {
+        searchWeather: function () {
+            console.log("天气查询")
+        }
+    },
+    methods: {
+
+    },
+
+});
