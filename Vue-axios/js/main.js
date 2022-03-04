ac2334175976c978373d3a5531103252ff5e787d
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-04 11:32:26 +0800

    Vue-axios 应用代码比较复杂的时候 将自己写的js代码和页面分离开来 然后再引用. 切记: 自己编写的js要在最后引用 否则会出现挂载失败情况

--- Vue-axios/js/main.js
+++ Vue-axios/js/main.js
@@ -12,11 +12,23 @@
 var vue = new Vue({
     el: "#app",
     data: {
-
+        city: "",
+        weatherList: [],
     },
     methods: {
         searchWeather: function () {
             console.log("天气查询");
+            console.log(this.city);
+            // 将this对象引用保存起来. 因为要进入axios中了. this会改变
+            var that = this;
+            axios.get("http://wthrcdn.etouch.cn/weather_mini?city=" + this.city)
+                .then(function (response) {
+                    console.log(response.data.data.forecast);
+                    that.weatherList = response.data.data.forecast;
+                })
+                .catch(function (err) {
+                    console.log(err);
+            });
         }
     },
 
