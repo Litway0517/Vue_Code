04cb6751ec5dddadd98b5476bb67b606efce5765
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-04 20:58:48 +0800

    Vue-music 完成自动为歌曲增加照片

--- Vue-music/js/main.js
+++ Vue-music/js/main.js
@@ -30,9 +30,14 @@
 let vue = new Vue({
     el: "#player",
     data: {
+        // 搜索框中的歌曲名称
         musicName: "",
+        // 歌曲列表
         searchMusicList: [],
+        // 歌曲URL
         musicUrl: "",
+        // 歌曲Img地址
+        musicImgUrl: "",
 
 
     },
@@ -43,7 +48,7 @@ let vue = new Vue({
             var that = this;
             axios.get("https://autumnfish.cn/search?keywords=" + this.musicName)
                 .then(function (response) {
-                    console.log(response.data.result.songs);
+                    console.log("歌曲数据 -> " + response.data.result.songs);
                     that.searchMusicList = response.data.result.songs;
                 })
                 .catch(function (err) {
@@ -56,14 +61,30 @@ let vue = new Vue({
             axios.get("https://autumnfish.cn/song/url?id=" + musicId)
                 .then(function (response) {
                     // 注意取数据的正确性. 不正确的 -> response.data.data.url.
-                    // 正缺的 -> response.data.data[0].url
-                    console.log(response.data.data[0].url);
+                    // 正确的取值顺序 -> response.data.data[0].url
+                    console.log("音乐播放地址 ->" + response.data.data[0].url);
                     that.musicUrl = response.data.data[0].url;
                 })
                 .catch(function (err) {
                     console.log(err);
                 });
+
+            // 调用加载图片功能
+            this.setMusicImg(musicId);
         },
+        // 设置播放时的图片
+        setMusicImg: function (musicId) {
+            var that = this;
+            axios.get("https://autumnfish.cn/song/detail?ids=" + musicId)
+                .then(function (response) {
+                    console.log("音乐图片地址 -> " + response.data.songs[0].al.picUrl);
+                    that.musicImgUrl = response.data.songs[0].al.picUrl;
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+
+        }
     }
 
 });
